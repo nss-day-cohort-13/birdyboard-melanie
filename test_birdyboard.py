2d3ea13a586d0ef8e19f4917484ef326c22d9af3
Melanie Baker <melanierbaker@yahoo.com>
2016-08-01 15:02:43 -0500

    start work on unittesting. Still working.

---
+++ test_birdyboard.py
@@ -0,0 +1,31 @@
+import unittest
+import birdyboard
+
+class TestBirdyBoard(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(self):
+    pass
+
+  def test_new_user(self):
+    pass
+
+  def test_select_user(self):
+    pass
+
+  def test_view_chirps(self):
+    pass
+
+  def test_public_chirp(self):
+    pass
+
+  def test_private_chirp(self):
+    pass
+
+
+
+
+
+
+  if __name__ == '__main__':
+  unittest.main()
